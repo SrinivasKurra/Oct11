fb34e2c1fd11a101afc9f92284ea851e6dab5cbc
Srinivas K <srinivask.react@gmail.com>
2023-10-11 07:31:00 +0530

    commit for new-feature

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h2> This is for GIT</h2>
-      <h3> This is for third commit</h3>
+      <h3> This is for new feature</h3>
     </div>
   );
 }
